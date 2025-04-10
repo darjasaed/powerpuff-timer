a0ff0814423d3a676b910e0f4e50a4acf76eb589
Darja Saed <darsa831@student.liu.se>
2025-04-11 00:22:34 +0200

    hosting

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/powerpuff-timer/',  // Set this to the repository name
   plugins: [react()],
 })
+
